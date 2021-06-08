1ca21444370844a361c3b5da0e661ea2b6c888f6
unknown <mjmanzano4@gmail.com>
2021-06-08 15:20:48 +0800

    First Commit

---
+++ TOrganizer/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace TOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExifToolDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ImagePath = new System.Windows.Forms.TextBox();
+            this.OpenFolder = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.Sort_Checkbox = new System.Windows.Forms.CheckBox();
+            this.Suffix_checkbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ExifToolDirectory
+            // 
+            this.ExifToolDirectory.Location = new System.Drawing.Point(12, 39);
+            this.ExifToolDirectory.Name = "ExifToolDirectory";
+            this.ExifToolDirectory.Size = new System.Drawing.Size(413, 20);
+            this.ExifToolDirectory.TabIndex = 0;
+            this.ExifToolDirectory.TextChanged += new System.EventHandler(this.ExifToolDirectory_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Exif Tool Directory:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Image Folder";
+            // 
+            // ImagePath
+            // 
+            this.ImagePath.Location = new System.Drawing.Point(12, 91);
+            this.ImagePath.Name = "ImagePath";
+            this.ImagePath.Size = new System.Drawing.Size(413, 20);
+            this.ImagePath.TabIndex = 5;
+            this.ImagePath.TextChanged += new System.EventHandler(this.ImagePath_TextChanged);
+            // 
+            // OpenFolder
+            // 
+            this.OpenFolder.Location = new System.Drawing.Point(443, 91);
+            this.OpenFolder.Name = "OpenFolder";
+            this.OpenFolder.Size = new System.Drawing.Size(103, 20);
+            this.OpenFolder.TabIndex = 6;
+            this.OpenFolder.Text = "Open Folder";
+            this.OpenFolder.UseVisualStyleBackColor = true;
+            this.OpenFolder.Click += new System.EventHandler(this.OpenFolder_Click);
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(12, 130);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 7;
+            this.Run.Text = "Sort";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Sort_Checkbox
+            // 
+            this.Sort_Checkbox.AutoSize = true;
+            this.Sort_Checkbox.Location = new System.Drawing.Point(120, 130);
+            this.Sort_Checkbox.Name = "Sort_Checkbox";
+            this.Sort_Checkbox.Size = new System.Drawing.Size(131, 17);
+            this.Sort_Checkbox.TabIndex = 8;
+            this.Sort_Checkbox.Text = "Sort Images to Folders";
+            this.Sort_Checkbox.UseVisualStyleBackColor = true;
+            this.Sort_Checkbox.CheckedChanged += new System.EventHandler(this.Sort_Checkbox_CheckedChanged);
+            // 
+            // Suffix_checkbox
+            // 
+            this.Suffix_checkbox.AutoSize = true;
+            this.Suffix_checkbox.Location = new System.Drawing.Point(288, 130);
+            this.Suffix_checkbox.Name = "Suffix_checkbox";
+            this.Suffix_checkbox.Size = new System.Drawing.Size(118, 17);
+            this.Suffix_checkbox.TabIndex = 9;
+            this.Suffix_checkbox.Text = "Add Temp as Suffix";
+            this.Suffix_checkbox.UseVisualStyleBackColor = true;
+            this.Suffix_checkbox.CheckedChanged += new System.EventHandler(this.Suffix_checkbox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 183);
+            this.Controls.Add(this.Suffix_checkbox);
+            this.Controls.Add(this.Sort_Checkbox);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.OpenFolder);
+            this.Controls.Add(this.ImagePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExifToolDirectory);
+            this.Name = "Form1";
+            this.Text = "TOrganizer v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ExifToolDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ImagePath;
+        private System.Windows.Forms.Button OpenFolder;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.CheckBox Sort_Checkbox;
+        private System.Windows.Forms.CheckBox Suffix_checkbox;
+    }
+}
+
---
+++ TOrganizer/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace TOrganizer
+{
+
+    public partial class Form1 : Form
+    {
+        const string config_filename = "config.txt";
+        const string _EXIFTOOL = "EXIFTOOLPATH";
+        const string _IMGPATH = "IMGPATH";
+        const string _SORTCHK = "SORT_CHECKED";
+        const string _TSUFFIX = "TEMP_SUFFIX";
+
+        string EXIFTOOLPATH = "";
+        string IMAGEFOLDER = "";
+
+        string EXIFARG = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+           Startup();
+        }
+
+
+        private void Startup()
+        {
+            try
+            {
+                    StreamReader sr = new StreamReader(config_filename);
+                    LoadConfig(sr);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                if(e.GetType().ToString() == "System.IO.FileNotFoundException")
+                {
+                    File.Create(config_filename);
+                }
+            }
+        }
+
+        private void LoadConfig(StreamReader sr)
+        {
+            string text;
+            text = sr.ReadToEnd();
+            sr.Close();
+            string[] lines = text.Split('\n');
+
+            foreach (string line in lines)
+            {
+                LoadVariables(line);
+            }
+        }
+
+        private void LoadVariables(string line)
+        {
+            string[] config = line.Split(new[] { ':' }, 2);
+            if (config.Length >= 2) {
+                switch (config[0].Trim()) {
+                    case _EXIFTOOL:
+                        EXIFTOOLPATH = config[1].Trim();
+                        ExifToolDirectory.Text = config[1].Trim();
+                        if (!File.Exists(EXIFTOOLPATH))
+                        {
+                            MessageBox.Show("Exiftool does not exist in the config path.");
+                        }
+                        break;
+                    case _IMGPATH:
+                        IMAGEFOLDER = config[1].Trim();
+                        ImagePath.Text = config[1].Trim();
+                        break;
+                    case _SORTCHK:
+                        Sort_Checkbox.Checked = get_bool(config[1].Trim());
+                        break;
+                    case _TSUFFIX:
+                        Suffix_checkbox.Checked = get_bool(config[1].Trim());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void ExifToolDirectory_TextChanged(object sender, EventArgs e)
+        {
+            string path = ExifToolDirectory.Text;
+            UpdateConfig(_EXIFTOOL, path);
+        }
+
+        private void ImagePath_TextChanged(object sender, EventArgs e)
+        {
+            IMAGEFOLDER = ImagePath.Text;
+            UpdateConfig(_IMGPATH, IMAGEFOLDER);
+        }
+
+        private void OpenFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    ImagePath.Text = fbd.SelectedPath;
+                    IMAGEFOLDER = fbd.SelectedPath;
+                    UpdateConfig(_IMGPATH, IMAGEFOLDER);
+                }
+            }
+        }
+
+        private void UpdateConfig(string config_name, string value)
+        {
+            string text = File.ReadAllText(config_filename);
+            string[] lines = text.Split('\n');
+
+            bool found_tag = false;
+
+            int x;
+            for (x = 0; x < lines.Length; x++)
+            {
+                string[] row = lines[x].Split(new[] { ':' }, 2);
+
+                if (row.Length >= 2)
+                {
+                    if (row[0].Trim() == config_name)
+                    {
+                        lines[x] = config_name + " : " + value;
+                        found_tag = true;
+                    }
+                }
+            }
+
+            if (found_tag == false)
+            {
+                Array.Resize(ref lines, lines.Length + 1);
+                lines[x] = (config_name + " : " + value);
+            }
+
+            text = "";
+            for(int y = 0; y < lines.Length; y++)
+            {
+                if (y == lines.Length - 1)
+                {
+                    text = text + lines[y];
+                }
+                else
+                {
+                    text = text + lines[y] + '\n';
+                }
+            }
+
+
+            File.WriteAllText(config_filename, text);
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+
+            if (IMAGEFOLDER != "")
+            {
+                 EXIFARG = "\"" + IMAGEFOLDER + @"\*.CR2" + "\"" + " -CameraTemperature" + " -csv > " + "\"" + IMAGEFOLDER + @"\temperature_log.csv" + "\"";
+
+                var process = new System.Diagnostics.Process
+                {
+                    StartInfo = new System.Diagnostics.ProcessStartInfo
+                    {
+                        FileName = EXIFTOOLPATH,
+                        Arguments = EXIFARG,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                string output = "";
+                int x = 0;
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    string line = process.StandardOutput.ReadLine();
+                    string[] row = line.Split(',');
+                    
+                    if (row.Length > 1 && x > 0)
+                    {
+                        output = output + line + '\n';
+                        string img_path = row[0];
+                        if (Sort_Checkbox.Checked)
+                        {
+                            img_path = SortImage(row[0], row[1]);
+                        }
+                        
+                        if (Suffix_checkbox.Checked && row[1].Trim() != "")
+                        {
+                            AddSuffix(img_path, row[1]);
+                        }
+                    }
+                    x++;
+                }
+                try
+                {
+                    File.WriteAllText(IMAGEFOLDER + @"\temperature_log.csv", output);
+                }
+                catch(Exception e2)
+                {
+                    Console.WriteLine("Exception: " + e2.GetType());
+                    if (e2.GetType().ToString() == "System.IO.FileNotFoundException")
+                    {
+                        MessageBox.Show("Exception: " + e2.GetType());
+                    }
+                }
+                MessageBox.Show("Operation completed successfully. " + x + " images sorted.");
+            }
+            else
+            {
+                MessageBox.Show("No Image folder has been set.");
+            }
+        }
+        private static string SortImage(string path, string folder)
+        {
+            string directory = System.IO.Path.GetDirectoryName(path);
+            string target_directory = directory + @"/" + folder;
+            string file_name = Path.GetFileName(path);
+            System.IO.Directory.CreateDirectory(target_directory);
+
+            string new_path = target_directory + @"/" + file_name;
+            System.IO.File.Move(path, new_path);
+            return new_path;
+        }
+
+        private static void AddSuffix(string path, string suffix)
+        {
+            string filename = Path.GetFileNameWithoutExtension(path);
+            string ext = Path.GetExtension(path);
+            string parent = Path.GetDirectoryName(path);
+
+            filename = filename + "_" + suffix.Replace(" ", "");
+
+            System.IO.File.Move(path, parent + @"\" + filename + ext);
+        }
+
+        private void Sort_Checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Sort_Checkbox.Checked)
+            {
+                UpdateConfig(_SORTCHK, "1");
+            }
+            else
+            {
+                UpdateConfig(_SORTCHK, "0");
+            }
+        }
+
+        private bool get_bool(string i)
+        {
+            int j = Int16.Parse(i);
+            if (j <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void Suffix_checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Suffix_checkbox.Checked)
+            {
+                UpdateConfig(_TSUFFIX, "1");
+            }
+            else
+            {
+                UpdateConfig(_TSUFFIX, "0");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Location of ExifTool";
+                ofd.DefaultExt = "exe";
+                ofd.Filter = "executable files (*.exe)|*.exe";
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.Multiselect = false;
+                if (EXIFTOOLPATH != "") {
+                    ofd.InitialDirectory = EXIFTOOLPATH;
+                        }
+
+                DialogResult result = ofd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                {
+                    ExifToolDirectory.Text = ofd.FileName;
+                    EXIFTOOLPATH = ofd.FileName;
+                    UpdateConfig(_EXIFTOOL, EXIFTOOLPATH);
+                }
+            }
+        }
+    }
+}
